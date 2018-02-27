5f062adee89fa441dbfa24b0cab41b3f292cad40
User <lehaazarenko@mail.ru>
2018-02-28 01:28:47 +0300

    unfinished solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let result = 0, k = 1, five;
+  
+  function findFive(b) {
+    let x, d = 2;
+    while (b > d) {
+      if (b % d === 0) {
+        b = b / d;
+        k++;
+      } else {
+        d++;
+        k = 1;
+      }
+    }
+    if (b > d) {
+      return b;
+    } else {
+      return d;
+    }
+    
+  }
+
+  function calc(num, x) {
+    return Math.floor(num / x);
+  } 
+  
+  five = findFive(base);
+  for (let i = 1; i < 11; i++) {
+  	let temp = Math.pow(five, i);
+  	if (number >= temp) {
+  	  result += calc(number, temp);
+  	} else {
+  	  break;
+  	}
+  }
+  result = Math.floor(result / k);
+  return result;
 }
